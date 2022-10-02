b5e8ee8d9b54068211d61469047607b84bf95caa
abhishekY2401 <abhiyd2401@gmail.com>
2022-10-02 23:42:50 +0530

    cors issue

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const corsOpts = {
   allowedHeaders: ["Content-Type"],
 };
 
-app.use(cors(corsOpts));
-
 const app = express();
 const bodyParser = require("body-parser");
 app.use(express.json());
+app.use(cors(corsOpts));
 
 app.use("/services", require("./controllers/services"));
 app.use("/contact", require("./controllers/contact"));
